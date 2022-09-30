a1e327a5536a4510ece0a7d01314150b7632028a
marlonecobos <manubio13@gmail.com>
2022-09-30 11:25:04 -0500

    v 0.0.1 First commit

---
+++ R/kuenm2.R
@@ -0,0 +1,16 @@
+#' kuenm2: Detailed Development of Ecological Niche Models
+#'
+#' kuenm2 helps with the development of critical phases of the
+#' ecological niche modeling process. Data preparation analyses and
+#' explorations can be done to prepare data. Model calibration (model selection)
+#' can be done by creating and testing several candidate models. Handy options
+#' for producing final models, evaluating such models, and assessing extrapolation
+#' risks are also included. Tools for post-modeling analyses are implemented to
+#' allow for further exploration of results.
+#'
+#'
+#' @section kuenm functions:
+#'
+#' @docType package
+#' @name kuenm2
+NULL
