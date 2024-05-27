0466145fb5549ae966ba197822877d6935872a23
wevertonbio <wevertonf1993@hotmail.com>
2024-05-27 15:46:51 -0300

    Function returns threshold, omission rate and addsampletobackground. Set seed to get same partitioning.

--- R/fit_selected_glmnetmx.R
+++ R/fit_selected_glmnetmx.R
@@ -12,7 +12,8 @@ fit_selected_glmnetmx <- function(calibration_results,
                                   ncores = 2,
                                   parallelType = "doSNOW",
                                   progress_bar = TRUE,
-                                  verbose = TRUE) {
+                                  verbose = TRUE,
+                                  seed = 42) {
   # #Args
   # to_export <- c("aic_nk", "aic_ws",
   #               "eval_stats","glmnet_mx",
@@ -45,7 +46,7 @@ fit_selected_glmnetmx <- function(calibration_results,
                           pr_bg = "pr_bg",
                           train_portion = train_portion,
                           n_replicates = n_replicates,
-                          method = rep_type)
+                          method = rep_type, seed = seed)
   } else {
       rep_data <- NULL
     }
@@ -90,7 +91,7 @@ fit_selected_glmnetmx <- function(calibration_results,
                            # ,
                            #     .export = c(to_export)
     ) %dopar% {
-      fit_best_model(x = x, dfgrid, calibration_results, data_x, n_replicates,
+      kuenm2:::fit_best_model(x = x, dfgrid, calibration_results, data_x, n_replicates,
                      rep_data)
     }
   } else { #Not in parallel (using %do%)
@@ -98,7 +99,7 @@ fit_selected_glmnetmx <- function(calibration_results,
     # Loop for com barra de progresso manual
     for (x in 1:n_tot) {
       # Execute a função fit_eval_models
-      best_models[[x]] <- fit_best_model(x = x, dfgrid, calibration_results,
+      best_models[[x]] <- kuenm2:::fit_best_model(x = x, dfgrid, calibration_results,
                                          data_x, n_replicates,
                                          rep_data)
 
@@ -176,7 +177,7 @@ fit_selected_glmnetmx <- function(calibration_results,
                            # ,
                            #    .export = c(to_export)
     ) %dopar% {
-      fit_best_model(x = x, dfgrid, calibration_results, data_x,
+      kuenm2:::fit_best_model(x = x, dfgrid, calibration_results, data_x,
                      n_replicates = 1)
     }
   } else { #Not in parallel (using %do%)
@@ -184,7 +185,7 @@ fit_selected_glmnetmx <- function(calibration_results,
     # Loop for com barra de progresso manual
     for (x in 1:n_models) {
       # Execute a função fit_eval_models
-      full_models[[x]] <- fit_best_model(x = x, dfgrid, calibration_results,
+      full_models[[x]] <- kuenm2:::fit_best_model(x = x, dfgrid, calibration_results,
                                          data_x, n_replicates = 1)
 
       # Sets the progress bar to the current state
@@ -205,13 +206,57 @@ fit_selected_glmnetmx <- function(calibration_results,
   if(parallel){
     parallel::stopCluster(cl) }
 
+  #Compute threshold
+  #Get models names
+  nm <- names(best_models)
+
+  #Get occurrences
+  occ <- calibration_results$calibration_data[
+    calibration_results$calibration_data$pr_bg == 1, -1]
+
+  #Predict models to occurrences - Per replicate
+  p_occ <- lapply(nm, function(x){
+    m_x <- best_models[[x]]
+    #Remove full model if replicates exist
+    if(any(grepl("Rep", names(m_x)))){
+      m_x$Full_model <- NULL
+    }
+    #Predict by replicate
+    p_r <- sapply(m_x, function(i){
+      predict.glmnet_mx(object = i, newdata = occ, type = "cloglog")
+    })
+    #Get mean and median
+    p_mean <- apply(p_r, 1, mean, na.rm=T)
+    p_median <- apply(p_r, 1, median, na.rm=T)
+    #Save in a list
+    p_res <- list(mean = p_mean,
+                  median = p_median)
+  })
+  names(p_occ) <- nm
+  #Get consensus
+  mean_consensus <- apply(sapply(p_occ, function(x) x$mean),
+                          1, mean, na.rm = TRUE)
+  median_consensus <- apply(sapply(p_occ, function(x) x$median),
+                          1, median, na.rm = TRUE)
+  consensus <- list(mean = mean_consensus,
+                    median = median_consensus)
+  #Merge list
+  p_occ <- c(p_occ, list(consensus = consensus))
+  #Calculate threshold
+  p_thr <- lapply(p_occ, function(model) {
+      lapply(model, calc_thr,
+             thr = calibration_results$omission_rate/100)
+    })
+
   #Final results
   res <- list(species = calibration_results$species,
               Models = best_models,
               calibration_data = calibration_results$calibration_data,
               selected_models = calibration_results$selected_models,
               weights = calibration_results$weights,
-              addsamplestobackground = calibration_results$addsamplestobackground)
+              addsamplestobackground = calibration_results$addsamplestobackground,
+              omission_rate = calibration_results$omission_rate,
+              thresholds = p_thr)
 
   #Write models?
   if(write_models){
@@ -222,84 +267,15 @@ fit_selected_glmnetmx <- function(calibration_results,
 } #End of function
 
 
-# #Test function
-# #Load functions
-# source("Functions/Metrics_Functions.R")
-# source("Functions/eval_m.R")
-# source("Functions/part_data.R")
-#
-# #Prediction to dataframe or raster
-# #In df, each replicate in a column
-#
-# #Select 3 best models from candidate results
-# cand_res <- read.csv("Models/Piper_fuligineum/candidate_results.csv")
-# selected_models <- cand_res %>%
-#   filter(is_concave == F, proc_pval.mean < 0.05,
-#          Omission_rate_at_5.mean <= 0.05) %>% slice_min(AIC, n =3)
-# occ_bg <- read.csv("Models/Piper_fuligineum/occ_bg.csv")
-#
-# #With kfold
-# best_model <- fit_best(data = occ_bg,
-#                        pr_bg = "pr_bg",
-#                        selected_models = selected_models,
-#                        var_categorical = NULL,
-#                        replicates = TRUE,
-#                        n_replicates = 4,
-#                        train_portion = 0.7,
-#                        rep_type = "kfold",
-#                        parallel = TRUE,
-#                        ncores = 5,
-#                        progress_bar = TRUE,
-#                        write_models = FALSE, #Write files?
-#                        out_dir = NULL, #Name of the folder to write final models
-#                        parallelType = "doSNOW",
-#                        verbose = TRUE)
-#
-# #With subsample
-# best_model2 <- fit_best(data = occ_bg,
-#                        pr_bg = "pr_bg",
-#                        selected_models = selected_models,
-#                        var_categorical = NULL,
-#                        replicates = TRUE,
-#                        n_replicates = 5,
-#                        train_portion = 0.7,
-#                        rep_type = "subsample",
-#                        parallel = TRUE,
-#                        ncores = 5,
-#                        progress_bar = TRUE,
-#                        write_models = FALSE, #Write files?
-#                        out_dir = NULL, #Name of the folder to write final models
-#                        parallelType = "doSNOW",
-#                        verbose = TRUE)
-# #With bootstrap
-# best_model3 <- fit_best(data = occ_bg,
-#                         pr_bg = "pr_bg",
-#                         selected_models = selected_models,
-#                         var_categorical = NULL,
-#                         replicates = TRUE,
-#                         n_replicates = 10,
-#                         train_portion = 0.7,
-#                         rep_type = "bootstrap",
-#                         parallel = TRUE,
-#                         ncores = 5,
-#                         progress_bar = TRUE,
-#                         write_models = FALSE, #Write files?
-#                         out_dir = NULL, #Name of the folder to write final models
-#                         parallelType = "doSNOW",
-#                         verbose = TRUE)
-# #With kfold, no parallel and writing results
-# best_model4 <- fit_best(data = occ_bg,
-#                         pr_bg = "pr_bg",
-#                         selected_models = selected_models,
-#                         var_categorical = NULL,
-#                         replicates = TRUE,
-#                         n_replicates = 4,
-#                         train_portion = 0.7,
-#                         rep_type = "kfold",
-#                         parallel = T,
-#                         ncores = 5,
-#                         progress_bar = TRUE,
-#                         write_models = TRUE, #Write files?
-#                         out_dir = "Models/Piper_fuligineum/Best_models/", #Name of the folder to write final models
-#                         parallelType = "doSNOW",
-#                         verbose = TRUE)
+# # #Test function
+# calibration_results = readRDS("../test_kuenm2/Candidate_and_Best_models.RDS")
+# n_replicates = 5
+# rep_type = "kfold"
+# train_portion = 0.7
+# write_models = FALSE #Write files?
+# file_name = NULL #Name of the folder to write final models
+# parallel = TRUE
+# ncores = 5
+# parallelType = "doSNOW"
+# progress_bar = TRUE
+# verbose = TRUE
