ec7cf9c85df69c541ff98dc6c04a9c41bd420db6
marlonecobos <manubio13@gmail.com>
2024-09-20 22:31:15 -0500

    first check documentaiton calibration_mxnet

--- R/calibration_glmnetmx.R
+++ R/calibration_glmnetmx.R
@@ -1,72 +1,126 @@
 #' Calibration, evaluation and selection of candidate models
+#'
 #' @description
 #' This function fits and validates candidate models using the data and grid of
 #' formulas prepared with `prepare_data()`. Then, it selects the best models
 #' based on concave curves (optional), omission rate, and AIC values.
 #'
-#' @param data an object of class `prepare_data` returned by the prepare_data() function.
-#' @param formula_grid an object of class `formula_grid` containing the grid of formulas returned by the calibration_grid_glmnetmx() function.
-#' @param test_concave (logical) whether to test for and remove candidate models presenting concave curves. Default is TRUE.
-#' @param addsamplestobackground (logical) whether to add to the background any presence sample that is not already there. Default is TRUE.
-#' @param use_weights (logical) whether to apply the weights present in the data. Default is FALSE.
-#' @param parallel (logical) whether to fit the candidate models in parallel. Default is FALSE.
-#' @param ncores (numeric) number of cores to use for parallel processing. Default is 1. This is only applicable if `parallel = TRUE`.
-#' @param parallel_type (character) the package to use for parallel processing: "doParallel" or "doSNOW". Default is "doSNOW". This is only applicable if `parallel = TRUE`.
-#' @param progress_bar (logical) whether to display a progress bar during processing. Default is TRUE.
-#' @param write_summary (logical) whether to save the evaluation results for each candidate model to disk. Default is FALSE.
-#' @param out_dir (character) the file name, with or without a path, for saving the evaluation results for each candidate model. This is only applicable if `write_summary = TRUE`.
-#' @param skip_existing_models (logical) whether to check for and skip candidate models that have already been fitted and saved in `out_dir`. This is only applicable if `write_summary = TRUE`. Default is FALSE.
-#' @param return_replicate (logical) whether to return the evaluation results for each replicate. Default if FALSE, meaning only the summary (mean and standard deviation) of the evaluation results will be returned.
-#' @param omrat_threshold (numeric) a value from 0 to 100 represeting the percentage of potential error (E) that the data due to any source of uncertainty. Default = 10.
-#' @param AIC (character) the type of AIC to be calculated: "ws" for AIC proposed by Warren and Seifert (2011), or "nk" for AIC proposed by Ninomiya and Kawano (2016). Default is "ws". See References for details.
-#' @param delta_aic the value of delta AIC used as a threshold to select models. Default is 2.
-#' @param allow_tolerance (logical) whether to allow selection of models with minimum values of omission rates even if their omission rate surpasses the `omrat_threshold`. This is only applicable if  all candidate models have omission rates higher than the `omrat_threshold`. Default is TRUE.
-#' @param tolerance (numeric) The value added to the minimum omission rate if it exceeds the `omrat_threshold`. If `allow_tolerance = TRUE`, selected models will have an omission rate equal to or less than the minimum rate plus this tolerance. Default is 0.01.
-#' @param verbose (logical) whether to display messages during processing. Default is TRUE.
+#' @usage
+#' calibration_glmnetmx(data, test_concave = TRUE,
+#'                      addsamplestobackground = TRUE, use_weights = FALSE,
+#'                      parallel = TRUE, ncores = 1, parallel_type = "doSNOW",
+#'                      progress_bar = TRUE, write_summary = FALSE,
+#'                      out_dir = NULL, skip_existing_models = FALSE,
+#'                      return_replicate = TRUE, omrat_threshold = 10,
+#'                      AIC = "ws", delta_aic = 2, allow_tolerance = TRUE,
+#'                      tolerance = 0.01, verbose = TRUE)
+#'
+#' @param data an object of class `prepare_data` returned by the prepare_data()
+#' function.
+#' @param formula_grid an object of class `formula_grid` containing the grid of
+#' formulas returned by the calibration_grid_glmnetmx() function.
+#' @param test_concave (logical) whether to test for and remove candidate models
+#' presenting concave curves. Default is TRUE.
+#' @param addsamplestobackground (logical) whether to add to the background any
+#' presence sample that is not already there. Default is TRUE.
+#' @param use_weights (logical) whether to apply the weights present in the
+#' data. Default is FALSE.
+#' @param parallel (logical) whether to fit the candidate models in parallel.
+#' Default is FALSE.
+#' @param ncores (numeric) number of cores to use for parallel processing.
+#' Default is 1. This is only applicable if `parallel = TRUE`.
+#' @param parallel_type (character) the package to use for parallel processing:
+#' "doParallel" or "doSNOW". Default is "doSNOW". This is only applicable if
+#' `parallel = TRUE`.
+#' @param progress_bar (logical) whether to display a progress bar during
+#' processing. Default is TRUE.
+#' @param write_summary (logical) whether to save the evaluation results for
+#' each candidate model to disk. Default is FALSE.
+#' @param out_dir (character) the file name, with or without a path, for saving
+#' the evaluation results for each candidate model. This is only applicable if
+#' `write_summary = TRUE`.
+#' @param skip_existing_models (logical) whether to check for and skip candidate
+#' models that have already been fitted and saved in `out_dir`. This is only
+#' applicable if `write_summary = TRUE`. Default is FALSE.
+#' @param return_replicate (logical) whether to return the evaluation results
+#' for each replicate. Default if FALSE, meaning only the summary (mean and
+#' standard deviation) of the evaluation results will be returned.
+#' @param omrat_threshold (numeric) a value from 0 to 100 representing the
+#' percentage of potential error (E) that the data due to any source of
+#' uncertainty. Default = 10.
+#' @param AIC (character) the type of AIC to be calculated: "ws" for AIC
+#' proposed by Warren and Seifert (2011), or "nk" for AIC proposed by Ninomiya
+#' and Kawano (2016). Default is "ws". See References for details.
+#' @param delta_aic the value of delta AIC used as a threshold to select models.
+#' Default is 2.
+#' @param allow_tolerance (logical) whether to allow selection of models with
+#' minimum values of omission rates even if their omission rate surpasses the
+#' `omrat_threshold`. This is only applicable if  all candidate models have
+#' omission rates higher than the `omrat_threshold`. Default is TRUE.
+#' @param tolerance (numeric) The value added to the minimum omission rate if it
+#' exceeds the `omrat_threshold`. If `allow_tolerance = TRUE`, selected models
+#' will have an omission rate equal to or less than the minimum rate plus this
+#' tolerance. Default is 0.01.
+#' @param verbose (logical) whether to display messages during processing.
+#' Default is TRUE.
+#'
 #' @importFrom parallel makeCluster stopCluster
 #' @importFrom doParallel registerDoParallel
 #' @importFrom doSNOW registerDoSNOW
 #' @importFrom foreach foreach `%dopar%`
 #' @importFrom utils setTxtProgressBar
+#'
+#' @export
+#'
 #' @return
 #' An object of class 'calibration_results' containing the following elements:
 #' - species: a character string with the name of the species.
-#' - calibration data: a data.frame containing a column (`pr_bg`) that identifies occurrence points (1) and background points (0), along with the corresponding values of predictor variables for each point.
-#' - formula_grid: data frame containing the calibration grid with possible formulas and parameters.
-#' - kfolds: a list of vectors with the indices of rows corresponding to each fold.
-#' - data_xy: a data.frame with the coordinates of occurrence and background points.
-#' - continuous_variables: a character indicanting the continuous varibles.
-#' - categorical_variables: a character indicanting the categorical varibles (if used).
-#' - weights: the numeric vector specifying weights for the occurrence records (if used).
-#' - pca: if a principal component analysis was performed on the predictor variables, a list of class "prcomp". See ?stats::prcomp() for details.
+#' - calibration data: a data.frame containing a column (`pr_bg`) that
+#' identifies occurrence points (1) and background points (0), along with the
+#' corresponding values of predictor variables for each point.
+#' - formula_grid: data frame containing the calibration grid with possible
+#' formulas and parameters.
+#' - kfolds: a list of vectors with row indices corresponding to each fold.
+#' - data_xy: a data.frame with occurrence and background coordinates.
+#' - continuous_variables: a character indicating the continuous variables.
+#' - categorical_variables: a character, categorical variable names (if used).
+#' - weights: a numeric vector specifying weights for data_xy (if used).
+#' - pca: if a principal component analysis was performed with variables, a list
+#' of class "prcomp". See ?stats::prcomp() for details.
 #' - model_type: the model type (glm or glmnet)
-#' - calibration_results: a list containing a data frame with all evaluation metrics for all replicates (if `return_replicate = TRUE`) and a summary of the evaluation metrics for each candidate model.
+#' - calibration_results: a list containing a data frame with all evaluation
+#' metrics for all replicates (if `return_replicate = TRUE`) and a summary of
+#' the evaluation metrics for each candidate model.
 #' - omission_rate: The omission rate determined by `omrat_threshold`.
-#' - addsampletobackground: a logical value indicating whether any presence sample not already in the background was added. Default is TRUE.
-#' - selected_models:  data frame with the ID and the summary of evaluation metrics for the selected models.
-#' - summary: A list containing the delta AIC values for model selection, and the ID values of models that failed to fit, had concave curves, non-significant pROC values, omission rates above the threshold, delta AIC values above the threshold, and the selected models.
-#' @usage calibration_glmnetmx(data, test_concave = TRUE,
-#'                             addsamplestobackground = TRUE, use_weights = FALSE,
-#'                             parallel = TRUE, ncores = 1, parallel_type = "doSNOW",
-#'                             progress_bar = TRUE, write_summary = FALSE,
-#'                             out_dir = NULL, skip_existing_models = FALSE,
-#'                             return_replicate = TRUE, omrat_threshold = 10,
-#'                             AIC = "ws", delta_aic = 2, allow_tolerance = TRUE,
-#'                             tolerance = 0.01, verbose = TRUE)
-#' @export
-#' @references
-#' Ninomiya, Yoshiyuki, and Shuichi Kawano. "AIC for the Lasso in generalized linear models." (2016): 2537-2560.
+#' - addsampletobackground: a logical value indicating whether any presence
+#' sample not already in the background was added. Default is TRUE.
+#' - selected_models:  data frame with the ID and the summary of evaluation
+#' metrics for the selected models.
+#' - summary: A list containing the delta AIC values for model selection, and
+#' the ID values of models that failed to fit, had concave curves,
+#' non-significant pROC values, omission rates above the threshold, delta AIC
+#' values above the threshold, and the selected models.
 #'
-#' Warren, D. L., & Seifert, S. N. (2011). Ecological niche modeling in Maxent: the importance of model complexity and the performance of model selection criteria. Ecological applications, 21(2), 335-342.
+#' @references
+#' Ninomiya, Yoshiyuki, and Shuichi Kawano. "AIC for the Lasso in generalized
+#' linear models." (2016): 2537-2560.
+#'
+#' Warren, D. L., & Seifert, S. N. (2011). Ecological niche modeling in Maxent:
+#' the importance of model complexity and the performance of model selection
+#' criteria. Ecological applications, 21(2), 335-342.
+#'
 #' @examples
-#' #Import occurrences
+#' # Import occurrences
 #' data(occ_data, package = "kuenm2")
-#' #Import variables
+#'
+#' # Import variables
 #' var <- terra::rast(system.file("extdata", "Current_variables.tif",
 #'                                package = "kuenm2"))
-#' #Use only variables 1, 2 and 3
+#'
+#' # Use only variables 1, 2 and 3
 #' var <- var[[1:3]]
-#' #Prepare data
+#'
+#' # Prepare data
 #' sp_swd <- prepare_data(model_type = "glmnet", occ = occ_data,
 #'                        species = occ_data[1, 1], x = "x", y = "y",
 #'                        spat_variables = var, mask = NULL,
@@ -81,7 +135,8 @@
 #'                        include_xy = TRUE,
 #'                        write_file = FALSE, file_name = NULL,
 #'                        seed = 1)
-#' #Calibrate models
+#'
+#' # Calibrate models
 #' m <- calibration_glmnetmx(data = sp_swd,
 #'                           test_concave = TRUE,
 #'                           parallel = FALSE,
@@ -99,7 +154,7 @@
 #'                           skip_existing_models = FALSE,
 #'                           verbose = TRUE)
 #' m
-#'
+
 calibration_glmnetmx <- function(data,
                                 test_concave = TRUE,
                                 addsamplestobackground = TRUE,
@@ -117,16 +172,7 @@ calibration_glmnetmx <- function(data,
                                 delta_aic = 2,
                                 allow_tolerance = TRUE,
                                 tolerance = 0.01,
-                                verbose = TRUE){
-
-  # #Args to parallel
-  # to_export <- c("aic_nk", "aic_ws", "eval_stats","glmnet_mx",
-  #                "maxnet.default.regularization", "omrat",
-  #                "predict.glmnet_mx", "empty_replicates",
-  #                "empty_summary", "hinge", "hingeval", "thresholds",
-  #                "thresholdval", "categorical", "categoricalval",
-  #                "fit_eval_concave", "fit_eval_models", "omrat_thr",
-  #                "omrat_threshold", "sel_best_models")
+                                verbose = TRUE) {
 
   #Check calibration data class and convert to dataframe if necessay
   if(is.matrix(data$calibration_data) | is.array(data$calibration_data)) {
