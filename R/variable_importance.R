e3f274af93eec0fb3aa185006333c7b72f7c3661
luisagi <ariasgiraldo.luisfelipe@gmail.com>
2025-03-14 16:32:24 +0100

    v0.0.9: typo corrected

--- R/variable_importance.R
+++ R/variable_importance.R
@@ -252,7 +252,7 @@ var_importance_indmx <- function(model, p, data, f, rm, algorithm,
 #' Summary plot for variable importance in models
 #'
 #' @description
-#' See detailes in \code{\link[enmpa]{plot_importance}}
+#' See details in \code{\link[enmpa]{plot_importance}}
 #'
 #' @usage
 #' plot_importance(x, xlab = NULL, ylab = "Relative contribution",
