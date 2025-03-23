e9e70944e63d8bff0de68d7fbb0d83adb4e80500
wevertonbio <wevertonf1993@hotmail.com>
2025-03-23 14:41:41 -0300

    replace spat_variables with raster_variables

--- R/explore_calibration_geo.R
+++ R/explore_calibration_geo.R
@@ -2,7 +2,7 @@
 #'
 #' @param data an object of class `prepare_data` returned by the
 #'            \code{\link{prepare_data}}() function.
-#' @param spat_variables (SpatRaster) predictor variables used for model calibration.
+#' @param raster_variables (SpatRaster) predictor variables used for model calibration.
 #' @param plot (logical) wheter to plot the SpatRaster. Default is TRUE.
 #'
 #' @return
@@ -16,7 +16,7 @@
 #'
 #' @export
 #' @importFrom terra extract NAflag levels trim crop plot
-#' @usage explore_calibration_geo(data, spat_variables, plot = TRUE)
+#' @usage explore_calibration_geo(data, raster_variables, plot = TRUE)
 #' @examples
 #' #Import raster layers
 #' var <- terra::rast(system.file("extdata", "Current_variables.tif",
@@ -26,7 +26,7 @@
 #' #Prepare data
 #' sp_swd <- prepare_data(model_type = "glmnet", occ = occ_data,
 #'                        species = occ_data[1, 1], x = "x", y = "y",
-#'                        spat_variables = var, mask = NULL,
+#'                        raster_variables = var, mask = NULL,
 #'                        categorical_variables = "SoilType",
 #'                        do_pca = FALSE, deviance_explained = 95,
 #'                        min_explained = 5, center = TRUE, scale = TRUE,
@@ -39,26 +39,36 @@
 #'                        write_file = FALSE, file_name = NULL,
 #'                        seed = 1)
 #' #Explore calibration data
-#' pbg <- explore_calibration_geo(data = sp_swd, spat_variables = var[[1]],
+#' pbg <- explore_calibration_geo(data = sp_swd, raster_variables = var[[1]],
 #'                                plot = FALSE)
-#'
 explore_calibration_geo <- function(data,
-                                    spat_variables,
+                                    raster_variables,
                                     plot = TRUE){
+
+  #Check data
+  if(!inherits(data, "prepared_data")){
+    stop("'data' must be a 'prepared_data' object, not ", class(data))
+  }
+
+  if(!inherits(raster_variables, "SpatRaster"))
+    stop("'raster_variables' must be a 'SpatRaster' object, not ", class(raster_variables))
+
+  if(!inherits(plot, "logical"))
+    stop("'plot' must be logical, not ", class(plot))
 
   #Extract cell values
   bg <- data$data_xy[data$calibration_data$pr_bg == 0,]
   p <- data$data_xy[data$calibration_data$pr_bg == 1,]
 
   #Get cells
-  bg_cells <- terra::extract(spat_variables[[1]], bg,
+  bg_cells <- terra::extract(raster_variables[[1]], bg,
                              cells = TRUE, ID = FALSE)$cell
-  p_cells <- terra::extract(spat_variables[[1]], p,
+  p_cells <- terra::extract(raster_variables[[1]], p,
                             cells = TRUE, ID = FALSE)$cell
   p_bg_cells <- intersect(bg_cells, p_cells)
 
   #Fill rasters
-  r <- (!is.na(spat_variables[[1]])) * 1
+  r <- (!is.na(raster_variables[[1]])) * 1
   terra::NAflag(r) <- 0 #Set NA
   r[bg_cells] <- 2
   r[p_cells] <- 3
@@ -68,7 +78,7 @@ explore_calibration_geo <- function(data,
                           category=c("Unused data", "Background", "Presence",
                                      "Presence & Background"))
   #Crop and trim final raster
-  r <- terra::trim(terra::crop(r, spat_variables[[1]], mask = TRUE))
+  r <- terra::trim(terra::crop(r, raster_variables[[1]], mask = TRUE))
 
   if(plot){
   terra::plot(r)}
