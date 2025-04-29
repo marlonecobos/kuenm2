39e5c0e955d3f74b6df200b9057fcbf846f3b827
marlonecobos <manubio13@gmail.com>
2025-04-29 12:43:53 -0400

    general update of dunctions, documentation improved, examples tested

--- R/partial_roc.R
+++ R/partial_roc.R
@@ -1,18 +1,20 @@
-#' Partial ROC calculation for candidate models
+#' Partial ROC calculation for multiple candidate models
 #'
 #' @description
-#' Applies partial ROC tests to model predictions
+#' Computes partial ROC tests for multiple candidate models.
 #'
-#' @usage partial_roc(formula_grid, data, omission_rate = 10,
-#'                    addsamplestobackground = TRUE, weights = NULL,
-#'                    algorithm = maxnet, parallel = FALSE, ncores = 1,
-#'                    parallel_option = "doSNOW", progress_bar = TRUE)
-#' @param formula_grid (data.frame) the formula grid of the candidate models
-#' to test.
+#' @usage
+#' partial_roc(formula_grid, data, omission_rate = 10,
+#'             addsamplestobackground = TRUE, weights = NULL,
+#'             algorithm = "maxnet", parallel = FALSE, ncores = NULL,
+#'             progress_bar = TRUE)
+#'
+#' @param formula_grid a data.frame with the grid of formulas defining the
+#' candidate models to test.
 #' @param data an object of class `prepared_data` returned by the
-#' \code{\link{prepare_data()}} function or an object of class
-#' calibration_results returned by the \code{\link{calibration()}} function.
-#' It contains the calibration data and kfolds.
+#' \code{\link{prepare_data}}() function or an object of class
+#' calibration_results returned by the \code{\link{calibration}}() function.
+#' It contains the calibration data and k-folds.
 #' @param omission_rate (numeric) values from 0 to 100 representing the
 #' percentage of potential error due to any source of uncertainty. This value is
 #' used to calculate the omission rate. Default is 10. See details.
@@ -25,9 +27,7 @@
 #' @param parallel (logical) whether to fit the candidate models in parallel.
 #' Default is FALSE.
 #' @param ncores (numeric) number of cores to use for parallel processing.
-#' Default is 1. This is only applicable if `parallel = TRUE`.
-#' @param parallel_option (character) the package to use for parallel processing:
-#' "doParallel" or "doSNOW". Default is "doSNOW". This is only applicable if
+#' Default is NULL and uses available cores - 1. This is only applicable if
 #' `parallel = TRUE`.
 #' @param progress_bar (logical) whether to display a progress bar during
 #' processing. Default is TRUE.
@@ -39,51 +39,53 @@
 #'
 #' @importFrom parallel makeCluster
 #' @importFrom utils setTxtProgressBar txtProgressBar
-#' @importFrom doParallel registerDoParallel
 #' @importFrom doSNOW registerDoSNOW
 #' @importFrom foreach foreach `%dopar%`
 #' @importFrom enmpa predict_glm proc_enm
 #'
 #' @details
-#' Partial ROC is calculated following Peterson et al. (2008) doi:10.1016/j.ecolmodel.2007.11.008.
+#' Partial ROC is calculated following Peterson et al. (2008)
+#' <doi:10.1016/j.ecolmodel.2007.11.008>.
 #'
 #' @examples
-#' # Import raster layers
-#' var <- terra::rast(system.file("extdata", "Current_variables.tif",
-#'                                package = "kuenm2"))
-#'
-#' # Import occurrences
-#' data(occ_data, package = "kuenm2")
-#'
-#'
-#' # Prepare data for maxnet model
-#' sp_swd <- prepare_data(algorithm = "maxnet", occ = occ_data,
-#'                        species = occ_data[1, 1], x = "x", y = "y",
-#'                        raster_variables = var,
-#'                        categorical_variables = "SoilType",
-#'                        n_background = 500,
-#'                        features = c("l", "q", "p", "lq", "lqp"),
-#'                        reg_mult = c(0.1, 1, 2, 3, 5))
+#' # Import prepared data to get model formulas
+#' data(sp_swd, package = "kuenm2")
 #'
 #' # Calculate proc for the first 5 candidate models
-#' res_proc <- partial_roc(formula_grid = sp_swd$formula_grid[1:5,],
-#'                         data = sp_swd,
-#'                         omission_rate = 10)
-#'
+#' res_proc <- partial_roc(formula_grid = sp_swd$formula_grid[1:2,],
+#'                         data = sp_swd, omission_rate = 10,
+#'                         algorithm = "maxnet")
+
 partial_roc <- function(formula_grid, data, omission_rate = 10,
                         addsamplestobackground = TRUE, weights = NULL,
-                        algorithm = "maxnet", parallel = FALSE, ncores = 1,
-                        parallel_option = "doSNOW", progress_bar = TRUE) {
+                        algorithm = "maxnet", parallel = FALSE, ncores = NULL,
+                        progress_bar = TRUE) {
 
-  # Parallelization setup
+  # error check
+  if (missing(formula_grid)) {
+    stop("Argument 'formula_grid' must be defined.")
+  }
+  if (missing(data)) {
+    stop("Argument 'data' must be defined.")
+  }
+  if (!inherits(formula_grid, "data.frame")) {
+    stop("Argument 'formula_grid' must be a 'data.frame' object.")
+  }
+  if (!inherits(data, "prepared_data")) {
+    stop("Argument 'data' must be a 'prepared_data' object.")
+  }
+
   n_tot <- nrow(formula_grid)
 
-  if (parallel) {
-    if (!(parallel_option %in% c("doSNOW", "doParallel"))) {
-      stop("Invalid parallel_option. Use 'doSNOW' or 'doParallel'.")
-    }
+  if (n_tot == 1 & parallel) {
+    parallel <- FALSE
+  }
 
-    if(ncores > n_tot){
+  if (parallel) {
+    if (is.null(ncores)) {
+      ncores <- max(1, parallel::detectCores() - 1)
+    }
+    if (ncores > n_tot) {
       ncores <- n_tot
     }
 
@@ -95,39 +97,32 @@ partial_roc <- function(formula_grid, data, omission_rate = 10,
   if (progress_bar) {
     pb <- utils::txtProgressBar(min = 0, max = n_tot, style = 3)
     progress <- function(n) utils::setTxtProgressBar(pb, n)
-    opts <- list(progress = progress)} else {opts <- NULL}
-
-  if (parallel & parallel_option == "doParallel") {
-    doParallel::registerDoParallel(cl)
-    opts <- NULL # Progress bar does not work with doParallel
-  }
-
-  if (parallel & parallel_option == "doSNOW") {
-    doSNOW::registerDoSNOW(cl)
-    if (isTRUE(progress_bar))
-      opts <- list(progress = progress)
-    else opts <- opts
+    opts <- list(progress = progress)
+  } else {
+    opts <- NULL
   }
 
   # Execute in parallel or sequentially
-  if(parallel){
+  if (parallel) {
+    doSNOW::registerDoSNOW(cl)
+
     res_proc <- foreach::foreach(
-      x = 1:n_tot,
-      .packages = c("glmnet", "enmpa"),
+      x = 1:n_tot, .packages = c("glmnet", "enmpa"),
       .options.snow = opts) %dopar% {
-        proc(x, formula_grid, data, omission_rate,
-             addsamplestobackground, weights,
-             algorithm)
+        proc(x, formula_grid, data, omission_rate, addsamplestobackground,
+             weights, algorithm)
       }
   } else {
     res_proc <- vector("list", length = n_tot)
     for (x in 1:n_tot) {
-      res_proc[[x]] <-
-        proc(x, formula_grid, data, omission_rate,
-             addsamplestobackground, weights,
-             algorithm)
+      res_proc[[x]] <- proc(x, formula_grid, data, omission_rate,
+                            addsamplestobackground, weights,
+                            algorithm)
+
       # Sets the progress bar to the current state
-      if(progress_bar) setTxtProgressBar(pb, x)
+      if (progress_bar) {
+        setTxtProgressBar(pb, x)
+      }
     }
   }
   #Bind results
