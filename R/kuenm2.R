c0d4a31d544383697780d3bbd936ad6846cbc3b7
marlonecobos <manubio13@gmail.com>
2025-04-29 12:33:02 -0400

    package documentation updated

--- R/kuenm2.R
+++ R/kuenm2.R
@@ -1,11 +1,23 @@
 #' kuenm2: Detailed Development of Ecological Niche Models
 #'
-#' kuenm2 helps to perform critical phases of the ecological niche modeling
-#' process. Data preparation analyses and explorations can be done to prepare
-#' data. Model calibration (model selection) can be done by creating and testing
-#' several candidate models. Handy options for producing final models,
-#' evaluating such models, and assessing extrapolation risks are also included.
-#' Tools for post-modeling analyses are implemented to allow further
-#' exploration of results.
+#' kuenm2 A new set of tools to help with the development of detailed ecological
+#' niche models using multiple algorithms. Pre-modeling analyses and
+#' explorations can be done to prepare data. Model calibration (model selection)
+#' can be done by creating and testing several candidate models. Handy options
+#' for producing final models with transfers are included. Other tools to
+#' assess extrapolation risks and variability in model transfers are also
+#' available.
+#'
+#' @section Functions by stage in the ENM process:
+#'
+#' - Data preparation: [initial_cleaning()], [advanced_cleaning()],
+#' [prepare_data()], [prepare_user_data()]
+#' - Model calibration: [calibration()], [select_models()]
+#' - Model exploration: [fit_selected()], [variable_importance()],
+#' [plot_importance()], [response_curve()], [bivariate_response()]
+#' - Model projection: [predict_selected()], [project_selected()]
+#' - Post-modeling: [projection_variability()], [projection_changes()],
+#' [projection_mop()]
+#'
 #'
 "_PACKAGE"
