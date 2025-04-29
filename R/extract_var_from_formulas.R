49f3823b21f70b1b0f244f77351868655d968fe5
marlonecobos <manubio13@gmail.com>
2025-04-29 12:40:00 -0400

    documentation updated, examples tested

--- R/extract_var_from_formulas.R
+++ R/extract_var_from_formulas.R
@@ -1,41 +1,52 @@
-#' Extract Predictor Names from Formulas
+#' Extract predictor names from formulas
 #'
-#' @usage extract_var_from_formulas(formulas, ...)
+#' @usage
+#' extract_var_from_formulas(formulas, ...)
+#'
 #' @param formulas (character or formula) model formulas.
 #' @param ... Arguments to pass to [all.vars()]
 #'
-#' @return A character vector or a list of the same length as the formulas,
-#' containing the variable names.
+#' @return
+#' A character vector or a list of the same length as `formulas`,
+#' containing the names of the predictors each formula.
 #'
 #' @export
 #'
 #' @importFrom stats terms as.formula
 #'
 #' @examples
-#' # Extract variables from the formula grid
-#' # Import an example of calibration results (output from the calibration function)
-#' data("calib_results_glmnet", package = "kuenm2")
-#' extract_var_from_formulas(formulas = calib_results_glmnet$formula_grid$Formulas)
+#' # Import an example of calibration results
+#' data(calib_results_maxnet, package = "kuenm2")
 #'
+#' # Extract predictor names
+#' vars <- extract_var_from_formulas(calib_results_maxnet$formula_grid$Formulas)
+
 extract_var_from_formulas <- function(formulas, ...) {
-  res <- sapply(formulas, function(object) {
-    if (inherits(object, "formula")) {
-      object <- stats::terms(object)
+  if (missing(formulas)) {
+    stop("Argument 'formulas' must be defined.")
+  }
+
+  res <- sapply(formulas, function(x) {
+    if (inherits(x, "formula")) {
+      x <- stats::terms(x)
     } else {
-      object <- stats::terms(stats::as.formula(object))
+      x <- stats::terms(stats::as.formula(x))
     }
-    y_index <- attr(object, "response")
+    y_index <- attr(x, "response")
 
     ## If there is something on the lhs of the formula,
     ## remove it and get vars
     if (y_index != 0) {
-      object[[2]] <- NULL
-      object <- stats::terms(object)
+      x[[2]] <- NULL
+      x <- stats::terms(x)
     }
-    all.vars(object, ...)
+    all.vars(x, ...)
   })
   names(res) <- NULL
+
   if(length(res) == 1) {
-    return(unlist(res))} else {
-  return(res)}
+    return(unlist(res))
+  } else {
+    return(res)
+  }
 }
